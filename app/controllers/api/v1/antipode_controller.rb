70b73d974ca1def0d1d60280600c385939fd35fc
jerry <jerry@jerrys-MBP.attlocal.net>
2020-04-20 12:01:19 -0400

    Add progress on antipode weather

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -3,7 +3,10 @@ class Api::V1::AntipodeController < ApplicationController
 	def show
 		location_service = GeoService.new(params[:location])
 		antipode_service = AntipodeService.new(location_service.fetch_latlong)
-		antipode_service.fetch_antipode
-		
+		antipode_latlong = antipode_service.fetch_antipode
+		weather_service = WeatherService.new(antipode_latlong)
+		message = weather_service.response
+		city = location_service.fetch_city(antipode_latlong)
+			
 	end
 end
--- app/services/geo_service.rb
+++ app/services/geo_service.rb
@@ -9,7 +9,8 @@ class GeoService
 	end
 
 	def fetch_city(lat_long)
-		reverse_get_json(lat_long)	
+		reverse_get_json(lat_long)[:results].first[:formatted_address]	
+	end
 
 	private
 
@@ -23,7 +24,13 @@ class GeoService
   end
 
 	def reverse_get_json(lat_long)
-		response = reverse_conn
+		lat_long = lat_long[:lat].to_s + ',' + lat_long[:long].to_s
+		response = reverse_conn(lat_long)
 		JSON.parse(response.body, symbolize_names: true)
 	end
+
+	def reverse_conn(lat_long)
+		Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat_long}&key=#{ENV['GOOGLE_API_KEY']}")
+	end
 end
+
